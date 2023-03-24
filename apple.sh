38a675fbd9125391de06bce26e765719a55acc46
Tejaswinihp <hp12tejaswini@gmail.com>
2023-03-24 13:42:09 +0000

    initial

---
+++ apple.sh
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+import java.util.Scanner;
+class Input {
